6da9439ab1ee107b6c8783375b75fa5e9217c533
Marius Rieder <marius.rieder@durchmesser.ch>
2014-11-20 07:06:37 +0100

    Add friendlyid and a ship list to the line

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,7 +1,12 @@
 class Line < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   validates :title,
     presence: true,
     length: { minimum: 2 },
     allow_blank: false,
     uniqueness: true
+
+  has_many :ship
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119171041) do
+ActiveRecord::Schema.define(version: 20141120053036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20141119171041) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "lines", ["slug"], :name => "index_lines_on_slug", :unique => true
 
   create_table "ports", force: true do |t|
     t.string   "title"
