9658c9c7747e76a3437617fe55e1f906feaff821
Marius Rieder <marius.rieder@durchmesser.ch>
2014-11-21 10:25:12 +0100

    Create custom path helpers

--- app/helpers/cruise_segments_helper.rb
+++ app/helpers/cruise_segments_helper.rb
@@ -1,2 +1,19 @@
 module CruiseSegmentsHelper
+
+  def cruise_segments_path
+    cruise_cruise_segments_path
+  end
+
+  def cruise_segment_path(segment)
+    cruise_cruise_segment_path(segment.cruise.friendly_id, segment)
+  end
+
+  def edit_cruise_segment_path(segment)
+    edit_cruise_cruise_segment_path(segment.cruise.friendly_id, segment)
+  end
+
+  def new_cruise_segment_path
+    new_cruise_cruise_segment_path(params[:cruise_id])
+  end
+
 end
