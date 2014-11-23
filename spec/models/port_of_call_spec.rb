cb8543cdb20adc23aea2a86bfd986990234efc00
Marius Rieder <marius.rieder@durchmesser.ch>
2014-11-23 11:21:10 +0100

    Use new syntax

--- spec/models/port_of_call_spec.rb
+++ spec/models/port_of_call_spec.rb
@@ -5,28 +5,30 @@ RSpec.describe PortOfCall, :type => :model do
 
     subject { create :port_of_call }
 
-    it 'should have valid factory' do
-      build(:port_of_call).should be_valid
+    it 'excpect have valid factory' do
+      port = build(:port_of_call)
+      expect(port).to be_valid
     end
 
     it 'arrive to be earlyer then leave' do
-     build(:port_of_call, arrive: Time.now, leave: Time.now-10).should_not be_valid
+      port = build(:port_of_call, arrive: Time.now, leave: Time.now-10)
+      expect(port).to_not be_valid
     end
 
     it 'arrive to be present' do
-      should validate_presence_of :arrive
+      expect validate_presence_of :arrive
     end
 
     it 'leave to be present' do
-      should validate_presence_of :leave
+      expect validate_presence_of :leave
     end
 
     it 'port to be present' do
-      should validate_presence_of :port
+      expect validate_presence_of :port
     end
 
     it 'ship to be present' do
-      should validate_presence_of :ship
+      expect validate_presence_of :ship
     end
   end
 end
