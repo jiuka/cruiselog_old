52f4a9ac4081990d4f4db5ccd4fa1ac0e478855d
Marius Rieder <marius.rieder@durchmesser.ch>
2014-11-23 11:21:36 +0100

    Add validations to PortOfCall

--- app/models/port_of_call.rb
+++ app/models/port_of_call.rb
@@ -2,6 +2,12 @@ class PortOfCall < ActiveRecord::Base
   belongs_to :ship
   belongs_to :port
 
+  validates :ship, presence: true
+  validates :port, presence: true
+  validates :arrive, presence: true
+  validates :leave, presence: true
+  validates :arrive , date: { before: :leave }
+
   def arrive=(s)
     self[:arrive] = s
     updateOrderDate
